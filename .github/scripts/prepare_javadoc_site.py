07475ed66ce1624313a5fa04b92002e7e016d439
Luca Previati <luca@previati.com>
2026-06-28 12:26:21 +0200

    refactor: remove automation compatibility wrappers

--- .github/scripts/prepare_javadoc_site.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Compatibility wrapper for nova_automation.docs.pages."""
-
-from nova_automation.docs.pages import *  # noqa: F401,F403
-from nova_automation.docs.pages import main as _main
-
-
-if __name__ == "__main__":
-    raise SystemExit(_main())
