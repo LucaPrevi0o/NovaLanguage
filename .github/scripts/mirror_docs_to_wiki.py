f74fe54558189737f85f305e39345bd71cd4c274
Luca Previati <luca@previati.com>
2026-06-28 12:26:09 +0200

    refactor: remove automation compatibility wrappers

--- .github/scripts/mirror_docs_to_wiki.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Compatibility wrapper for nova_automation.docs.wiki."""
-
-from nova_automation.docs.wiki import *  # noqa: F401,F403
-from nova_automation.docs.wiki import main as _main
-
-
-if __name__ == "__main__":
-    raise SystemExit(_main())
